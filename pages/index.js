85b7f1fded81a40817efbccb8064359bfdbe5bf6
3douhao <3douhao@gmail.com>
2020-10-29 16:19:23 +0800

    before make cardContent display flex

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,6 @@ import Card from '@material-ui/core/Card'
 import Typography from '@material-ui/core/Typography'
 import CardMedia from '@material-ui/core/CardMedia'
 import CardContent from '@material-ui/core/CardContent'
-import Grid from '@material-ui/core/Grid'
 import CardActions from '@material-ui/core/CardActions'
 import Button from '@material-ui/core/Button'
 
